6222ef7b51eca6a73739b227fd538dc23bd0ad0a
janikh87 <58213168+janikh87@users.noreply.github.com>
2021-07-02 21:02:46 +0200

    Update api.php
    fix

--- api.php
+++ api.php
@@ -1,7 +1,7 @@
 <?php
 require_once('inc/config.php');
 function custom_autoloader($class) {
-  include 'app/' . $class . '.php';
+  include 'App/' . $class . '.php';
 }
 spl_autoload_register('custom_autoloader');
 (new NumberApiController())->process();
